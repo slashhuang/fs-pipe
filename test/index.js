2d478624fe078a8c9876076ffc7b27eb94303dec
slashhuang <627284482@qq.com>
2017-02-20 19:33:16 +0800

    update test

--- test/index.js
+++ test/index.js
@@ -3,4 +3,16 @@
  * 测试用例
  */
 
- var fsPipe = require('fs-pipe');
+
+const path = require('path');
+const fsPipe = require('fs-pipe');
+const inst =   new fsPipe();
+
+let sourceStream =path.resolve(process.cwd(),".babelrc");
+let destStream =path.resolve(process.cwd(),"example","hello");
+
+
+inst.src(sourceStream)
+     .pipe(destStream)
+     // .empty(LogStreamPath)
+     .final(()=>console.log('file stream done'));
