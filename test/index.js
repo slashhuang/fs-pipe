3e8a6e375295ab0d60ce4e269e8fbc665293ef6f
slashhuang <627284482@qq.com>
2017-02-20 20:08:49 +0800

    add ignore

--- test/index.js
+++ test/index.js
@@ -9,10 +9,12 @@ import FsPiper from '../lib/index';
 const inst =   new FsPiper();
 let getFile = (...name)=> path.resolve.apply(null,[process.cwd(),...name])
 let sourceStream = getFile(".babelrc");
-let destStream = getFile("example","hello");
+let destStream1 = getFile("example","hello");
+let destStream2 = getFile("example","hello1");
 let emptyStream = getFile("test","readme.md");
 
 inst.src(sourceStream)
-     .pipe(destStream)
+     .pipe(destStream1)
+     .pipe(destStream2)
      .empty(emptyStream)
      .final(()=>console.log('file stream done'));
